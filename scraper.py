41150b8149e7c1070bd49d750d33767782903886
Duncan <chienweiwhite@gmail.com>
2022-05-10 19:49:59 +0800

    3.4 爬取子網頁資料

--- scraper.py
+++ scraper.py
@@ -11,4 +11,16 @@ title = soup.find_all('span', {'class':'lx-stream-post__header-text gs-u-align-m
 title_list = []
 for i in title:
     title_list.append(i.text)
-print(title_list)
+
+urls = soup.find_all('a', {'class':'qa-heading-link lx-stream-post__header-link'})
+url_title = 'https://www.bbc.com/'
+
+tag_list = []
+for i in urls:
+    sub_respon = requests.get(url_title + i.get('href'))
+    soup = BeautifulSoup(sub_respon.text, 'html.parser')
+    sub_title = soup.find_all('li', {'class':'bbc-1msyfg1 e1hq59l0'})
+    for i in sub_title:
+        tag_list.append(i.text)
+
+print(tag_list)
