36874739e9210dc63dbb93cc8af8ce48e2bea50a
Duncan <chienweiwhite@gmail.com>
2022-05-16 09:23:14 +0800

    3.6 提升爬取效率_grequests

--- scraper.py
+++ scraper.py
@@ -1,9 +1,17 @@
-import requests
+import grequests
 from bs4 import BeautifulSoup
+import time
 
-for page in range(1,4):
-    re = requests.get(f'https://www.bbc.com/zhongwen/trad/topics/c83plve5vmjt/page/{page}')
-    soup = BeautifulSoup(re.text, 'html.parser')
+
+start_time = time.time()
+
+links = [f'https://www.bbc.com/zhongwen/trad/topics/c83plve5vmjt/page/{page}' for page in range(1,4)]
+
+reqs = (grequests.get(link) for link in links)
+resps = grequests.imap(reqs, grequests.Pool(3))
+
+for index, resp in enumerate(resps):
+    soup = BeautifulSoup(resp.text, 'html.parser')
 
     title = soup.find_all('span', {'class':'lx-stream-post__header-text gs-u-align-middle'})
 
@@ -12,16 +20,21 @@ for page in range(1,4):
         title_list.append(i.text)
 
     urls = soup.find_all('a', {'class':'qa-heading-link lx-stream-post__header-link'})
-    url_title = 'https://www.bbc.com/'
+    sub_links = ['https://www.bbc.com/' + url.get('href') for url in urls]
+    sub_reqs = (grequests.get(sub_link) for sub_link in sub_links)
+    sub_resps = grequests.imap(sub_reqs, grequests.Pool(10))
+    
 
     tag_list = []
-    for i in urls:
-        sub_respon = requests.get(url_title + i.get('href'))
-        soup = BeautifulSoup(sub_respon.text, 'html.parser')
+    for sub_resp in sub_resps:
+        soup = BeautifulSoup(sub_resp.text, 'html.parser')
         sub_title = soup.find_all('li', {'class':'bbc-1msyfg1 e1hq59l0'})
         for i in sub_title:
             tag_list.append(i.text)
 
-    print(f'第{page}頁')
+    print(f'第{index+1}頁')
     print(title_list)
     print(tag_list)
+
+end_time = time.time()
+print(f'總共花費{end_time-start_time}秒')
